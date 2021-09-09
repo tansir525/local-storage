3100fe9628df1bf34e82146eafb2ebaad94628bf
tansir525 <tansirorick525@gmail.com>
2021-09-09 17:21:48 +0800

    js update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const addItem = () => {
   // display in the ui
   displayProduct(name);
   // add to local storage
+  addProductToCart(name);
 
   nameField.value = "";
 };
@@ -13,4 +14,22 @@ const displayProduct = (name) => {
   const li = document.createElement("li");
   li.innerText = name;
   ul.appendChild(li);
+};
+
+const getCart = () => {
+  const cart = localStorage.getItem("cart");
+  let cartobj;
+  if (cart) {
+    cartobj = JSON.parse(cart);
+  } else {
+    cartobj = {};
+  }
+  return cartobj;
+};
+
+const addProductToCart = (name) => {
+  const cart = getCart();
+  cart[name] = 1;
+  const cartStringfy = JSON.stringify(cart);
+  localStorage.setItem("cart", cartStringfy);
 };
